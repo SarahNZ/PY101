29f15f8673456da0228f55bb1877a99d89a918af
Sarah Stenfert <sarah.stenfert@gmail.com>
2024-09-03 11:02:32 +1200

    Completed 3 more exercises in Lesson 1 'Easy2' section

---
+++ lesson1/easy2/square_an_argument.py
@@ -0,0 +1,12 @@
+def multiply(num1, num2):
+    return num1 * num2
+
+def square(num):
+    return multiply(num, num)
+
+# Examples
+print(multiply(5, 3) == 15)  # True
+
+print(square(5) == 25)   # True
+print(square(-8) == 64)  # True
+
