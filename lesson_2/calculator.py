8d3e3e5550fbfbf7342472ae3140a64ad2f0dfe4
Sarah Stenfert <sarah.stenfert@gmail.com>
2024-09-19 14:04:48 +1200

    Completed Calculator Bonus Exercises

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,5 +1,14 @@
-def prompt(message):
-    print(f"==> {message}")
+# LANGUAGE = 'en'
+LANGUAGE = 'es'
+
+import json
+
+def prompt(key):
+    message = messages(key, LANGUAGE)
+    print(f' => {message}')
+
+def messages(message, lang='en'):
+    return MESSAGES[lang][message]
 
 def invalid_number(number_str):
     # try/except statement catches the ValueError exception when number_str
@@ -7,54 +16,73 @@ def invalid_number(number_str):
     # returns False if number_str can be converted to an integer, True if it
     # cannot
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
     
     return False
 
-print('Welcome to the Calculator!')
+# Now 'MESSAGES' contains the contents of the JSON file as a Python dictionary
+# or list
 
-# Ask the user for the first number
-prompt("What's the first number? ")
-number1 = input()
-
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number.")
+def calculation():
+    # Ask the user for the first number
+    prompt('first_number')
     number1 = input()
 
-# Ask the user for the second number
-prompt("What's the second number? ")
-number2 = input()
+    while invalid_number(number1):
+        prompt('invalid_number')
+        number1 = input()
 
-while invalid_number(number2):
-    prompt("Hmm... that doesn't look like a valid number.")
+    # Ask the user for the second number
+    prompt('second_number')
     number2 = input()
 
-# print(f'{number1} {number2}')
+    while invalid_number(number2):
+        prompt('invalid_number')
+        number2 = input()
 
-# Ask the user for an operation to perform
-prompt('What operation would you like to perform?\n1) Add '
-                  '2) Subtract 3) Multiply 4) Divide\n')
-operation = input()
-
-while operation not in ["1", "2", "3", "4"]:
-    prompt('You must choose 1, 2, 3, or 4')
+    # Ask the user for an operation to perform
+    prompt('operation')
     operation = input()
 
-# Perform the operation on the two numbers
-match operation:
-    case '1':   
-        output = int(number1) + int(number2)
-    case '2':
-        output = int(number1) - int(number2)
-    case '3':
-        output = int(number1) * int(number2)
-    case '4':
-        output = int(number1) / int(number2)
+    while operation not in ['1', '2', '3', '4']:
+        prompt('invalid_operation')
+        operation = input()
 
-# Print the result to the terminal
-print(f"The result is: {output}")
+    # Perform the operation on the two numbers
+    match operation:
+        case '1':   
+            output = float(number1) + float(number2)
+        case '2':
+            output = float(number1) - float(number2)
+        case '3':
+            output = float(number1) * float(number2)
+        case '4':
+            output = float(number1) / float(number2)
+
+    output = round(output, 2)
+
+    # Print the result to the terminal
+    print(f"The result is: {output}\n")
+
+# Open the JSON file for reading
+with open('calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
+
+# print(messages('welcome'))
+# or can do
+print(messages('welcome'))
+
+while True:
+    calculation()
+    # Ask the user if they want to perform another calculation
+    prompt(messages('repeat_operation'))
+    answer = input()
+    # if answer and answer[0].lower() != 'y':
+    # Not using above line of code as yes is 't' in Spanish
+    if answer != '1':
+        break
 
 # Test Cases
 # Addition
