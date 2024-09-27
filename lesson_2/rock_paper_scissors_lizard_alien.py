b27fedf3b3cfe0e507c4039179dcb12f0b37afc0
Sarah Stenfert <sarah.stenfert@gmail.com>
2024-09-27 15:51:24 +1200

    Refactored Rock Paper Scissors program based on feedback from code review

--- lesson_2/rock_paper_scissors_lizard_alien.py
+++ lesson_2/rock_paper_scissors_lizard_alien.py
@@ -3,13 +3,13 @@ import random
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'alien']
 FIRST_LETTER_OF_VALID_CHOICES = ['r', 'p', 's', 'l', 'a']
 
-WINNING_COMBOS = {
+PLAYER_WINNING_COMBOS = {
     'rock': ['scissors', 'lizard'], 
     'paper': ['rock', 'alien'],
     'scissors': ['paper', 'lizard'],
     'lizard': ['alien', 'paper'],
     'alien': ['scissors', 'rock'],
-}
+}   
 
 player_score = 0
 computer_score = 0
@@ -20,32 +20,20 @@ def prompt(message):
 def display_winner_of_game(player_choice, computer_choice):
     if player_wins(player_choice, computer_choice):
         prompt("You win!")
-        update_scoreboard(1, 0)
+        global player_score
+        player_score += 1
     elif player_choice == computer_choice:
         prompt("It's a tie!")
     else:
         prompt("Computer wins!")
-        update_scoreboard(0, 1)
+        global computer_score
+        computer_score += 1
 
 def player_wins(player_choice, computer_choice):
-    return computer_choice in WINNING_COMBOS[player_choice]
-
-def update_scoreboard(player_points, computer_points):
-    global player_score
-    player_score += player_points
-
-    global computer_score
-    computer_score += computer_points
-
-def reset_scores_to_zero():
-    global player_score
-    player_score = 0
-
-    global computer_score
-    computer_score = 0
+    return computer_choice in PLAYER_WINNING_COMBOS[player_choice]
 
 while True:
-    prompt(f'Enter the first letter of your choice: '
+    prompt(f'Hi Eva. Enter the first letter of your choice: '
            f'{", ".join(VALID_CHOICES)}')
     player_choice = input().lower()
 
@@ -74,11 +62,13 @@ while True:
 
     if player_score == 3:
         prompt("You won three games, so you won the match!")
-        reset_scores_to_zero()
+        player_score = 0
+        computer_score = 0
 
     elif computer_score == 3:
         prompt("The computer won three games, so they won the match!")
-        reset_scores_to_zero()
+        player_score = 0
+        computer_score = 0
     
     while True:
         prompt("Do you want to play again (y/n)?")
